f94515e8f24ee2e8cbe48f5a78a6a9702bdd26b9
DongWook <lsy0566@naver.com>
2020-06-02 23:37:33 +0900

    0602

---
+++ src/test/java/com/victolee/signuplogin/SignuploginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.victolee.signuplogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SignuploginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
